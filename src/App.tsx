386ae16a8c38d4895365f865dba3ccc72873f5eb
GalaxyNextDoor <aadithya2022@gmail.com>
2026-07-20 14:20:46 +0530

    Blue film

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,14 @@
-import Student from "./Student"
 // import Like from "./Like"
+import BlueFilm from "./blue_film"
 import Formy from "./form/Formy"
 
 export default function App(){
-  let student = [
-    {
-      name:"Aadi",
-      age : 20, 
-      course:"HTML"
-    },
-    {
-      name:"bharath",
-      age: 30,
-      course :"CSS"
-    },
-    {
-      name:"santhosh",
-      age: 900,
-      course :"CSS"
-    },
-    {
-      name:"pavan",
-      age: 3000,
-      course :"CSS"
-    },
-    {
-      name:"koushik",
-      age: 1,
-      course :"CSS"
-    }
-  ]
+
   return(
     <div className="App">
       {/* <Like></Like> */}
 
-      <h1 style={{
-        color:"rgb(0,0,256)"
-
-      }
-      }>
-        BLUE
-      </h1><h1>FILM</h1>
-      <p style={
-        {
-          color:"red"
-        }
-      }>by sandeep reddy vanga</p>
+      <BlueFilm/>
       <Formy/>
       
       
---
+++ src/blue_film.tsx
@@ -0,0 +1,18 @@
+export default function BlueFilm(){
+    return(
+        <div>
+        <h1 style={{
+        color:"rgb(0,0,256)"
+
+      }
+      }>
+        BLUE
+      </h1><h1>FILM</h1>
+      <p style={
+        {
+          color:"red"
+        }
+      }>by sandeep reddy vanga</p>
+      </div>
+    )
+}
