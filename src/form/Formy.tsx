202a1f3ce505207d0cc1beceabe4b529c0d23833
GalaxyNextDoor <aadithya2022@gmail.com>
2026-07-20 14:07:43 +0530

    submit form

--- src/form/Formy.tsx
+++ src/form/Formy.tsx
@@ -9,21 +9,32 @@ const Formy = () => {
             gender:""
         }
     ])
+    let handleChange = (e:any) => {
+        let {name, value} = e.target;
+        setForm({...form, [name]:value})
+    }
+    let handleSubmit = (e:any) => {
+        e.preventDefault();
+        console.log(form);
+    }
     return (
         <div>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Name</label>
-                <input type="text" name="username" id="username" />
+                <input type="text" name="username" id="username"  onChange={handleChange}/>
                 <br />
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
+                <input type="email" name="email" id="email" onChange={handleChange} />
                 <br />
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
+                <input type="password" name="password" id="password" onChange={handleChange} />
                 <br />
                 <label htmlFor="gender">Gender</label>
-                <input type="radio" name="gender" id="male" value="male" />Male
-                <input type="radio" name="gender" id="female" value="female" />Female
+                <input type="radio" name="gender" id="male" value="male" onChange={handleChange} />Male
+                <input type="radio" name="gender" id="female" value="female" onChange={handleChange} />Female
+                <div>
+                    <button>Submit</button>
+                </div>
             </form>
             </div>
     )
